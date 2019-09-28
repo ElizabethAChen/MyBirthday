393b07e0eba9bc570fb642feb7e30386f938c182
ElizabethAChen <elizabethchen@csu.fullerton.edu>
2019-09-28 12:47:29 -0700

    Initial commit

---
+++ app/src/main/java/com/kingbart/mybirthday/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.kingbart.mybirthday
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity: AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        birthdayChecker.setOnClickListener {buClickEvent()}
+    }
+
+    fun buClickEvent(){
+        val userName: String = inputName.text.toString()
+        val userDOBM:String = inputBdayMonth.text.toString()
+        val userDOBD:String = inputBdayDay.text.toString()
+        val userDOB = userDOBM.plus(userDOBD)
+        val month:String=Calendar.getInstance().get(Calendar.MONTH).toString()
+        val day:String=Calendar.getInstance().get(Calendar.DAY_OF_MONTH).toString()
+        val currentDate = month.plus(day)
+        when {
+            userName.isEmpty() -> userMessage.text = ("You did not enter your name")
+            userDOBM.isEmpty() -> userMessage.text = ("You did not enter you birthday month")
+            userDOBD.isEmpty() -> userMessage.text = ("You did not enter you birthday day")
+            else -> if (userDOB == currentDate) {
+                userMessage.text = "Congrats, $userName! Happy Birthday!"
+                val resId = R.drawable.birthdaycake
+                imageView.setImageResource(resId)
+            } else {
+                userMessage.text = "Sorry, $userName! It's not your birthday"
+            }
+        }
+    }
+}
